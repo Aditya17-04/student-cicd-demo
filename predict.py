d1bbf064c2f1436c699151e7299af196653aff5c
Aditya Suryawanshi <adityasuryawanshi1704@gmail.com>
2026-08-28 17:32:56 +0530

    Changes in predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pickle
 
 
@@ -16,13 +17,20 @@ projects = int(input("Enter Number of Projects: "))
 internship = int(input("Internship (1 = Yes, 0 = No): "))
 
 
-student = [[
+student = pd.DataFrame([[
     cgpa,
     attendance,
     coding_score,
     projects,
     internship
-]]
+]], columns=[
+    "CGPA",
+    "Attendance",
+    "CodingScore",
+    "Projects",
+    "Internship"
+])
+
 
 prediction = model.predict(student)[0]
 
